e8b44cb92024fd5a92fe7c459e25031e64a5c40a
moutis <35399827+moutis@users.noreply.github.com>
2025-07-26 20:10:56 -0400

    Update keynames_kyria.h

--- config/include/keynames_kyria.h
+++ config/include/keynames_kyria.h
@@ -88,3 +88,7 @@
 #define RH2 47
 #define RH3 48
 #define RH4 49
+
+#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4  LF0 LF1 // left-hand keys
+#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4  RF0 RF1 // right-hand keys
+#define THUMBS LH4 LH3 LH2 LH1 LH0 RH0 RH1 RH2 RH3 RH4                              // thumb keys
