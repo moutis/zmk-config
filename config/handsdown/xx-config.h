d646be4b924ebdeb1497db664dd519f6ab5e5e7c
moutis <35399827+moutis@users.noreply.github.com>
2025-07-29 09:19:15 -0400

    All behaviors

--- config/handsdown/xx-config.h
+++ config/handsdown/xx-config.h
@@ -10,17 +10,17 @@
             ╰──────────╯ ╰───────────╯
 */
 
-#define HD_adapt_file "handsdown/xx-adapt.dtsi"  // the related adaptive keys file
+#define HD_adapt_file "handsdown/vf-adapt.dtsi"  // the related adaptive keys file
 #define HD_combo_file "handsdown/xx-combos.dtsi" // the related combo file
 
 #define HD_Layer_label "HD-Vf"
-#define HD_LT &kp X           &kp W           &ak_M           &ak_G           &kp J
-#define HD_LM &hml RCTRL S    &hml RALT C     &hml RGUI N     &hml RSHFT T    &kp K
-#define HD_LB &kp F           &kp P           &kp L           &kp D           &kp V
-#define HD_LH                                                 &kp BSPC        &lt 0 R
+#define HD_LT &ak_X           &ak_W           &ak_M           &ak_G           &ak_J
+#define HD_LM &hml RCTRL S    &hml RALT C     &hml RGUI N     &hml RSHFT T    &ak_K
+#define HD_LB &ak_F           &ak_P           &ak_L           &ak_D           &ak_V
+#define HD_LH                                                 &ak_BSPC        &lt 0 R
 
-#define HD_RT &kp HASH        &kp DOT         &kp FSLH        &kp DQT         &kp SQT
-#define HD_RM &kp COMMA       &hmr LSHFT A    &hmr LGUI E     &hmr LALT I     &mt_H LCTRL H
-#define HD_RB &kp MINUS       &kp U           &kp O           &kp Y           &kp B
+#define HD_RT &ak_HASH        &ak_DOT         &ak_FSLH        &ak_DQT         &ak_SQT
+#define HD_RM &CmmaSemi       &hmr LSHFT A    &hmr LGUI E     &hmr LALT I     &mt_H LCTRL H
+#define HD_RB &ak_MINUS       &ak_U           &ak_O           &ak_Y           &ak_B
 #define HD_RH &lt 0 SPACE     &lt 0 RETURN
 
