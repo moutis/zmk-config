91e036f0b34c393f017a9edf668ba713b58f1d6e
moutis <35399827+moutis@users.noreply.github.com>
2025-07-29 00:47:30 -0400

    N48

--- config/handsdown/xx-config.h
+++ config/handsdown/xx-config.h
@@ -14,13 +14,13 @@
 #define HD_combo_file "handsdown/xx-combos.dtsi" // the related combo file
 
 #define HD_Layer_label "HD-Vf"
-#define HD_LT &kp X           &kp W           &kp M           &kp G           &kp J
+#define HD_LT &kp X           &kp W           &ak_M           &ak_G           &kp J
 #define HD_LM &hml RCTRL S    &hml RALT C     &hml RGUI N     &hml RSHFT T    &kp K
 #define HD_LB &kp F           &kp P           &kp L           &kp D           &kp V
 #define HD_LH                                                 &kp BSPC        &lt 0 R
 
 #define HD_RT &kp HASH        &kp DOT         &kp FSLH        &kp DQT         &kp SQT
-#define HD_RM &kp COMMA       &hmr LSHFT A    &hmr LGUI E     &hmr LALT I     &hmr LCTRL H
+#define HD_RM &kp COMMA       &hmr LSHFT A    &hmr LGUI E     &hmr LALT I     &mt_H LCTRL H
 #define HD_RB &kp MINUS       &kp U           &kp O           &kp Y           &kp B
 #define HD_RH &lt 0 SPACE     &lt 0 RETURN
 
