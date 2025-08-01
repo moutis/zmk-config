b8112b9fcd80327d89993db4af09ee616e850449
moutis <35399827+moutis@users.noreply.github.com>
2025-08-01 12:23:17 -0400

    Update handsdown.h

--- config/handsdown/handsdown.h
+++ config/handsdown/handsdown.h
@@ -6,34 +6,34 @@
  * common configuration defs
  *
  *
+ *
+ *  which HD alpha variation are we using?
+ *
+ *  nu = neu (no thumb alpha)    * RSNT does not use thumb
+ *  au = gold (T on thumb)       * RSND most inward rolling
+ *  ag = silver (L on thumb)     * RSNT LOW sfbs, but high repeat on thumb
+ *  bz = bronze (H on thumb)     * RSNT lightest thumb use w/H-digraph combos
+ *  cr = chromium (C on thumb)   * RSNT typically doesn't use H-digraph combos
+ *  cs = cesium (C on thumb)     * RSNT lowest scissors, – lowest thumb w/o H-digraph combos
+ *  pm = promethium (R on thumb) * SNTH typically doesn't use H-digraph combos
+ *  rh = rhodium (R on thumb)    * SCNT typically doesn't use H-digraph combos
+ *  ti = titanium (R on thumb)   * SCNT typically doesn't use H-digraph combos
+ *  vf = vibranium (R on thumb, f in lower left, b on right) - SCNT "canonical" best overall V#
+ *  vv = vibranium (R on thumb, v in lower left, f on right) - SCNT lowest SFBs, more inner column/scissors
+ *  vx = vibranium (R on thumb, x in lower left, f on right) - SCNT best pinky balancing, more inner column/scissors
+ *  vb = vibranium (R on thumb, b in lower left, f on right) - CSNT good pinky, mb is a problem
+ *  mi = mithril (R on thumb)
+ *  xx = experimental
+ *
  */
-//
-// which HD alpha variation are we using?
-//
-//
-// nu = neu (no thumb alpha)    * RSNT does not use thumb
-// au = gold (T on thumb)       * RSND most inward rolling
-// ag = silver (L on thumb)     * RSNT LOW sfbs, but high repeat on thumb
-// bz = bronze (H on thumb)     * RSNT lightest thumb use w/H-digraph combos
-// cr = chromium (C on thumb)   * RSNT typically doesn't use H-digraph combos
-// cs = cesium (C on thumb)     * RSNT lowest scissors, – lowest thumb w/o H-digraph combos
-// pm = promethium (R on thumb) * SNTH typically doesn't use H-digraph combos
-// rh = rhodium (R on thumb)    * SCNT typically doesn't use H-digraph combos
-// ti = titanium (R on thumb)   * SCNT typically doesn't use H-digraph combos
-// vf = vibranium (R on thumb, f in lower left, b on right) - SCNT "canonical" best overall V#
-// vv = vibranium (R on thumb, v in lower left, f on right) - SCNT lowest SFBs, more inner column/scissors
-// vx = vibranium (R on thumb, x in lower left, f on right) - SCNT best pinky balancing, more inner column/scissors
-// vb = vibranium (R on thumb, b in lower left, f on right) - CSNT good pinky, mb is a problem
-// mi = mithril (R on thumb)
-// xx = experimental
-//
+
 #define stringy(s) #s
 #define HD_FILENAME(dir, hd, file) stringy(##dir ##hd ##file)
 
 #ifndef HANDSDOWN       // this may have be defined in build.yml i.e. cmake-args: -HANDSDOWN=Pm
-#define HANDSDOWN pm    // HD layout variation 2 letter code
+#define HANDSDOWN vf    // HD layout variation 2 letter code from above
 #endif
 
-#define HD_config_file   HD_FILENAME(handsdown/, HANDSDOWN, -config.dtsi)
-#define HD_adapt_file    HD_FILENAME(handsdown/, HANDSDOWN, -adapt.dtsi)
-#define HD_combo_file    HD_FILENAME(handsdown/, HANDSDOWN, -combos.dtsi)
+#define HD_config_file   HD_FILENAME(handsdown/, ##HANDSDOWN, -config.dtsi)
+#define HD_adapt_file    HD_FILENAME(handsdown/, ##HANDSDOWN, -adapt.dtsi)
+#define HD_combo_file    HD_FILENAME(handsdown/, ##HANDSDOWN, -combos.dtsi)
