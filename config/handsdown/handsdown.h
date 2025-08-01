24369c37fda7a5078d5a2642c0f6dec33a32c1ef
moutis <35399827+moutis@users.noreply.github.com>
2025-08-01 14:00:12 -0400

    Update handsdown.h

--- config/handsdown/handsdown.h
+++ config/handsdown/handsdown.h
@@ -39,7 +39,8 @@
 #define __xstr(s) #s
 #define stringy(s) __xstr(s)
 
-#define HD_config_file   stringy(" HD_DIR HD_LAYOUT HD_CONF ")
+#define cf   stringy(HD_DIR HD_LAYOUT HD_CONF)
+#define HD_config_file stringy(cf)
 #define HD_adapt_file    stringy(HD_DIR HD_LAYOUT HD_ADPT)
 #define HD_combo_file    stringy(HD_DIR HD_LAYOUT HD_CMBO)
  
