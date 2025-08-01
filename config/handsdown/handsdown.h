072add2f0fe3d662ff815fd18e4128cf1a2b1b52
moutis <35399827+moutis@users.noreply.github.com>
2025-08-01 09:24:08 -0400

    abstracting #define HD_layout

---
+++ config/handsdown/handsdown.h
@@ -0,0 +1,14 @@
+/*
+ * handsdown.h
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * common configuration defs
+ *
+ *
+ */
+
+#define HD_config_file  "handsdown/##HD_layout##-config.dtsi"  // get the HD variation file
+#define HD_adapt_file   "handsdown/##HD_layout##-adapt.dtsi"   // the related adaptive keys file
+#define HD_combo_file   "handsdown/##HD_layout##-combos.dtsi"  // the related combo file
+
