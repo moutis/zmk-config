87778802abe7a7aaf9cbaf8b9b6dbfb4b9b39f38
moutis <35399827+moutis@users.noreply.github.com>
2025-08-14 13:12:48 -0400

    Update keynames_ffkb.h

--- config/keymaps/keynames_ffkb.h
+++ config/keymaps/keynames_ffkb.h
@@ -77,7 +77,7 @@
 #define RH1 35
 #define RH2 36
 #define RH3 37
-#defile RH4 38
+#define RH4 38
 
 #define KEYS_L LT0 LT1 LT2 LT3 LT4  LM0 LM1 LM2 LM3 LM4 CM1 LB0 LB1 LB2 LB3 LB4  // left-hand keys
 #define KEYS_R RT0 RT1 RT2 RT3 RT4  RM0 RM1 RM2 RM3 RM4 CM1 RB0 RB1 RB2 RB3 RB4  // right-hand keys
